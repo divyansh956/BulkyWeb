f4642c151a2d1767ac5bf5109819e56933da3ee6
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-13 14:44:10 +0530

    Read category data form db

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -11,5 +11,12 @@ namespace BulkyWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category { Id = 1, Name = "KoKo1", DisplayOrder = 1 },
+                                                    new Category { Id = 2, Name = "KoKo2", DisplayOrder = 2 },
+                                                    new Category { Id = 3, Name = "KoKo3", DisplayOrder = 3 });
+        }
     }
 }
