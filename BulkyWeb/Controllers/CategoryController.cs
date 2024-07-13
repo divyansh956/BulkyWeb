5dbfb4c51466fca18dac4d1635334ec4a26f490b
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-13 16:53:13 +0530

    Create category in db

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -4,17 +4,43 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            var objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
-        }
-    }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			var objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			//if (obj.Name == obj.DisplayOrder.ToString())
+			//{
+			//	ModelState.AddModelError("Name", "Category Name and Display Order cannot be the same.");
+			//}
+			//if (obj.Name != null && obj.Name.ToLower() == "admin")
+			//{
+			//	ModelState.AddModelError("", "You cannot add admin as Category Name.");
+			//}
+
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
+	}
 }
--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,12 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyWeb.Models
 {
-    public class Category
-    {
-        public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        public int DisplayOrder { get; set; }
-    }
+	public class Category
+	{
+		public int Id { get; set; }
+		[Required]
+		[MaxLength(30)]
+		[DisplayName("Category Name")]
+		public string Name { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1,100,ErrorMessage = "Display Order must be between 1 and 100.")]
+		public int DisplayOrder { get; set; }
+	}
 }
