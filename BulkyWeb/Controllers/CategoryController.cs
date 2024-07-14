bb28592d1a7198aa31bb9dc6841b61b14896ea35
Divyansh Saxena <divyanshsxn998@gmail.com>
2024-07-14 20:39:39 +0530

    Moving files from monolith project to separate projects: DataAccess, Models, Utilitiy

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,102 +1,102 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
-    public class CategoryController : Controller
-    {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-        public IActionResult Index()
-        {
-            var objCategoryList = _db.Categories.ToList();
-            return View(objCategoryList);
-        }
+	public class CategoryController : Controller
+	{
+		private readonly ApplicationDbContext _db;
+		public CategoryController(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+		public IActionResult Index()
+		{
+			var objCategoryList = _db.Categories.ToList();
+			return View(objCategoryList);
+		}
 
-        public IActionResult Create()
-        {
-            return View();
-        }
+		public IActionResult Create()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        public IActionResult Create(Category obj)
-        {
-            //if (obj.Name == obj.DisplayOrder.ToString())
-            //{
-            //	ModelState.AddModelError("Name", "Category Name and Display Order cannot be the same.");
-            //}
-            //if (obj.Name != null && obj.Name.ToLower() == "admin")
-            //{
-            //	ModelState.AddModelError("", "You cannot add admin as Category Name.");
-            //}
+		[HttpPost]
+		public IActionResult Create(Category obj)
+		{
+			//if (obj.Name == obj.DisplayOrder.ToString())
+			//{
+			//	ModelState.AddModelError("Name", "Category Name and Display Order cannot be the same.");
+			//}
+			//if (obj.Name != null && obj.Name.ToLower() == "admin")
+			//{
+			//	ModelState.AddModelError("", "You cannot add admin as Category Name.");
+			//}
 
-            if (ModelState.IsValid)
-            {
-                _db.Categories.Add(obj);
-                _db.SaveChanges();
-                TempData["Success"] = "Category created successfully";
-                return RedirectToAction("Index", "Category");
-            }
-            return View();
-        }
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				TempData["Success"] = "Category created successfully";
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
 
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Category? categoryFromDb = _db.Categories.Find(id);
-            if (categoryFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(categoryFromDb);
-        }
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
 
-        [HttpPost]
-        public IActionResult Edit(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Categories.Update(obj);
-                _db.SaveChanges();
-                TempData["Success"] = "Category updated successfully";
-                return RedirectToAction("Index", "Category");
-            }
-            return View();
-        }
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Update(obj);
+				_db.SaveChanges();
+				TempData["Success"] = "Category updated successfully";
+				return RedirectToAction("Index", "Category");
+			}
+			return View();
+		}
 
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Category? categoryFromDb = _db.Categories.Find(id);
-            if (categoryFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(categoryFromDb);
-        }
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
 
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? id)
-        {
-            Category? obj = _db.Categories.Find(id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _db.Categories.Remove(obj);
-            _db.SaveChanges();
-            TempData["Success"] = "Category deleted successfully";
-            return RedirectToAction("Index", "Category");
-        }
-    }
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["Success"] = "Category deleted successfully";
+			return RedirectToAction("Index", "Category");
+		}
+	}
 }
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,4 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,9 +12,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -25,7 +25,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
